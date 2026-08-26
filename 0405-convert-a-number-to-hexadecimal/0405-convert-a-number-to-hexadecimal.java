a91518d14b0506f6ca7dbc26741c80e9d3f1d477
Vijay Singh <vijaygariya300@gmail.com>
2026-08-26 18:59:54 +0200

    Time: 0 ms (100%), Space: 42.6 MB (46.39%) - LeetHub

---
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String toHex(int num) {
+
+        if (num == 0) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder(8);
+        char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+        while (num != 0) {
+
+            sb.append(hex[num & 0b1111]);
+            num >>>= 4;
+        }
+
+        return sb.reverse().toString();
+    }
+}
